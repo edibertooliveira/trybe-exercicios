fd76a454e36fe226164a22b99d6888eec03dddab
Ediberto Oliveira <edibertooliveira@aol.com>
2020-10-04 09:45:05 -0300

    exercises 2-1

--- Bloco_1/1-5/hello.sh
+++ Bloco_1/1-5/hello.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo "Ok, funcionou!"
-read NOME
-echo "Seu nome é $NOME"
+VARIAVEL='Beber Votar Viajar Sair Casar'
+
+echo 'Muito prazer, '$1
+read -p "Qual a sua idade?" IDADE
+
+if [ $IDADE -lt 18 ]
+then
+  echo 'Não pode beber!!'
+else
+  for opcao in $VARIAVEL
+  do
+  echo "Agora você ja pode $opcao"
+  done
+fi
