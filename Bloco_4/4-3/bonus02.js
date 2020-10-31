59164f074ef608541f71c6a6d56f470a778f9872
Ediberto Oliveira <edibertooliveira@aol.com>
2020-10-31 08:49:31 -0300

    chore: eslint corrector

--- Bloco_4/4-3/bonus02.js
+++ Bloco_4/4-3/bonus02.js
@@ -1,4 +1,4 @@
-/* 
+/*
 6- Faça um programa que diz se um número definido numa variável é primo ou não.
 Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja,
 a divisão dele com quaisquer outros números dá resto diferente de zero.
@@ -7,12 +7,12 @@ vai precisar de fazer uma checagem a cada iteração e armazenar os resultados e
 */
 
 let divisors;
-let numberToCheck = 5;
+const numberToCheck = 5;
 
 for (let number = 2; number < numberToCheck && divisors !== 1; number += 1) {
   divisors = 0;
-  if (numberToCheck % number == 0) divisors += 1;
+  if (numberToCheck % number === 0) divisors += 1;
 }
 
-if (divisors === 0) console.log(numberToCheck + " é primo");
-else console.log(numberToCheck + " não é primo");
+if (divisors === 0) console.log(`${numberToCheck} é primo`);
+else console.log(`${numberToCheck} não é primo`);
--- Bloco_4/4-3/exec01.js
+++ Bloco_4/4-3/exec01.js
@@ -1,13 +1,11 @@
-/* 
-1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1,
- imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
-*/
-let n = 5;
-let padrao = "";
-for (let i = 0; i < n; i++) {
-  padrao += "*";
+/* 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1,
+imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo: */
+const n = 5;
+let padrao = '';
+for (let i = 0; i < n; i += 1) {
+  padrao += '*';
 }
 console.log(padrao);
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i += 1) {
   console.log(padrao);
 }
