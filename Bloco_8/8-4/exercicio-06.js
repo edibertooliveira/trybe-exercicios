bb8f3932574173669dc60b03e43041e021013fb4
Ediberto Oliveira <edibertooliveira@aol.com>
2020-12-01 19:22:56 -0300

    prep: block_8 lesson 8-4 exercises 6

---
+++ Bloco_8/8-4/exercicio-06.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([palio, fiat, age]) => {
+  return {
+    name: palio,
+    brand: fiat,
+    year: age,
+  };
+};
+
+assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
+assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
+assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
