c0f5207303f8067a4747847a5958a41b6cf5d39e
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-09 17:39:47 -0300

    fix: exec 16-5

--- Bloco_16/16-5/exercise-test-with-redux-fetch/src/App.js
+++ Bloco_16/16-5/exercise-test-with-redux-fetch/src/App.js
@@ -1,21 +1,25 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchDog } from './store';
+import useFetch from './hook/useFetch';
+
 
 function App({ isFetching, src, fetchDog }) {
+  const { request, data, loading } = useFetch();
+  useEffect(()=> request('https://dog.ceo/api/breeds/image/random'),[])
   return (
-    isFetching ? <p>Loading</p>
+    loading ? <p>Loading</p>
       : (
         <div style={{ width: 500 }}>
           <button
             style={{ display: 'block' }}
-            onClick={fetchDog}
+            onClick={()=> request('https://dog.ceo/api/breeds/image/random')}
             type="button"
           >
             Novo Doguinho
           </button>
-          <img style={{ width: '100%' }} src={src} alt="dog" />
+          <img style={{ width: '100%' }} src={data.message} alt="dog" />
         </div>
       )
   );
